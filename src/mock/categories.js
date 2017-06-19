d80df2076dec000b6a454dd82822d4e000d571f6
Candice <wuqiong910718@163.com>
2017-06-19 13:49:41 +0800

    Init project.

---
+++ src/mock/categories.js
@@ -0,0 +1,519 @@
+const menu =
+[
+  {
+    'id': 1,
+    'tree': {
+      'id': 1,
+      'desc': '宝宝奶粉',
+      'desc2': null,
+      'level': 'level1',
+      'logo': null,
+      'popular': null,
+      'nodes': [
+        {
+          'id': 1,
+          'tree': {
+            'id': 1,
+            'desc': '牛奶粉',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/0.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 157,
+                'tree': {
+                  'id': 157,
+                  'desc': null,
+                  'desc2': 'a2 PLATINUM',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/1.png',
+                  'popular': 157,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 7,
+                'tree': {
+                  'id': 7,
+                  'desc': '德国爱他美',
+                  'desc2': 'Aptamil',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/2.png',
+                  'popular': 7,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 161,
+                'tree': {
+                  'id': 161,
+                  'desc': '爱他美白金',
+                  'desc2': 'Aptamil pro',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/3.png',
+                  'popular': 161,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 158,
+                'tree': {
+                  'id': 158,
+                  'desc': '澳洲爱他美',
+                  'desc2': 'Aptamil',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/4.png',
+                  'popular': 158,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 8,
+                'tree': {
+                  'id': 8,
+                  'desc': '荷兰牛栏',
+                  'desc2': 'Nutrilon',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/5.png',
+                  'popular': 8,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 166,
+                'tree': {
+                  'id': 166,
+                  'desc': '英国牛栏',
+                  'desc2': 'Cow&Cate',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/6.png',
+                  'popular': 166,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 9,
+                'tree': {
+                  'id': 9,
+                  'desc': '德国喜宝有机',
+                  'desc2': 'HiPP',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/7.png',
+                  'popular': 9,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 11,
+                'tree': {
+                  'id': 11,
+                  'desc': '德国喜宝益生菌',
+                  'desc2': 'HiPP',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/8.png',
+                  'popular': 11,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 13,
+                'tree': {
+                  'id': 13,
+                  'desc': '澳洲贝拉米',
+                  'desc2': 'BELLMAYS',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/0.png',
+                  'popular': 13,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 14,
+                'tree': {
+                  'id': 14,
+                  'desc': '荷兰美素',
+                  'desc2': 'Hero Baby',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/1.png',
+                  'popular': 14,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 12,
+                'tree': {
+                  'id': 12,
+                  'desc': '德国特福芬',
+                  'desc2': 'Topfer',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/2.png',
+                  'popular': 12,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        },
+        {
+          'id': 2,
+          'tree': {
+            'id': 2,
+            'desc': '羊奶粉',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/3.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 10,
+                'tree': {
+                  'id': 10,
+                  'desc': '德国泓乐',
+                  'desc2': 'Holle',
+                  'level': null,
+                  'logo': '../images/iconGrid/4.png',
+                  'popular': 10,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  {
+    'id': 2,
+    'tree': {
+      'id': 2,
+      'desc': '纸尿裤',
+      'desc2': null,
+      'level': 'level1',
+      'logo': null,
+      'popular': null,
+      'nodes': [
+        {
+          'id': 4,
+          'tree': {
+            'id': 4,
+            'desc': '纸尿裤',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/5.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 16,
+                'tree': {
+                  'id': 16,
+                  'desc': '花王',
+                  'desc2': 'Merries',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/6.png',
+                  'popular': 16,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 17,
+                'tree': {
+                  'id': 17,
+                  'desc': '大王',
+                  'desc2': 'Goon',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/7.png',
+                  'popular': 17,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 18,
+                'tree': {
+                  'id': 18,
+                  'desc': '尤妮佳',
+                  'desc2': 'Moony',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/8.png',
+                  'popular': 18,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        },
+        {
+          'id': 29,
+          'tree': {
+            'id': 29,
+            'desc': '湿巾',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/0.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 49,
+                'tree': {
+                  'id': 49,
+                  'desc': '贝亲',
+                  'desc2': 'Pigeon',
+                  'level': null,
+                  'logo': '../images/iconGrid/1.png',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 16,
+                'tree': {
+                  'id': 16,
+                  'desc': '花王',
+                  'desc2': 'Merries',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/2.png',
+                  'popular': 16,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  {
+    'id': 3,
+    'tree': {
+      'id': 3,
+      'desc': '辅食营养',
+      'desc2': null,
+      'level': 'level1',
+      'logo': null,
+      'popular': null,
+      'nodes': [
+        {
+          'id': 10,
+          'tree': {
+            'id': 10,
+            'desc': '宝宝营养',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/3.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 55,
+                'tree': {
+                  'id': 55,
+                  'desc': '铁元',
+                  'desc2': 'Salus',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/4.png',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 114,
+                'tree': {
+                  'id': 114,
+                  'desc': '挪帝克',
+                  'desc2': 'NORDIC NATURALS',
+                  'level': 'level3',
+                  'logo': '',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 106,
+                'tree': {
+                  'id': 106,
+                  'desc': '',
+                  'desc2': 'Sudocrem',
+                  'level': 'level3',
+                  'logo': '',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 153,
+                'tree': {
+                  'id': 153,
+                  'desc': '美赞臣',
+                  'desc2': null,
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/4.png',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 77,
+                'tree': {
+                  'id': 77,
+                  'desc': '',
+                  'desc2': 'Ddrops',
+                  'level': 'level3',
+                  'logo': '',
+                  'popular': null,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 34,
+                'tree': {
+                  'id': 34,
+                  'desc': '童年时光',
+                  'desc2': 'ChildLife',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/5.png',
+                  'popular': 34,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 35,
+                'tree': {
+                  'id': 35,
+                  'desc': '',
+                  'desc2': 'Bio Island',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/6.png',
+                  'popular': 35,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 73,
+                'tree': {
+                  'id': 73,
+                  'desc': '佳思敏',
+                  'desc2': 'Natures Way',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/7.png',
+                  'popular': 73,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 22,
+                'tree': {
+                  'id': 22,
+                  'desc': '美林',
+                  'desc2': 'Mellin',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/8.png',
+                  'popular': 22,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        },
+        {
+          'id': 5,
+          'tree': {
+            'id': 5,
+            'desc': '米粉面食',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/0.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 20,
+                'tree': {
+                  'id': 20,
+                  'desc': '世界最好',
+                  'desc2': 'Earths best',
+                  'level': null,
+                  'logo': '../images/iconGrid/1.png',
+                  'popular': 20,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 21,
+                'tree': {
+                  'id': 21,
+                  'desc': '嘉宝',
+                  'desc2': 'Gerber',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/2.png',
+                  'popular': 21,
+                  'nodes': null
+                }
+              },
+              {
+                'id': 22,
+                'tree': {
+                  'id': 22,
+                  'desc': '美林',
+                  'desc2': 'Mellin',
+                  'level': 'level3',
+                  'logo': '../images/iconGrid/4.png',
+                  'popular': 22,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        },
+        {
+          'id': 6,
+          'tree': {
+            'id': 6,
+            'desc': '蔬果泥',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/5.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 22,
+                'tree': {
+                  'id': 22,
+                  'desc': '美林',
+                  'desc2': 'Mellin',
+                  'level': null,
+                  'logo': '../images/iconGrid/6.png',
+                  'popular': 22,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        },
+        {
+          'id': 8,
+          'tree': {
+            'id': 8,
+            'desc': '鱼泥肉泥',
+            'desc2': null,
+            'level': 'level2',
+            'logo': '../images/iconGrid/7.png',
+            'popular': null,
+            'nodes': [
+              {
+                'id': 116,
+                'tree': {
+                  'id': 116,
+                  'desc': '简尼',
+                  'desc2': 'Jensens',
+                  'level': null,
+                  'logo': '../images/iconGrid/8.png',
+                  'popular': 116,
+                  'nodes': null
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+];
+exports.menu = menu
